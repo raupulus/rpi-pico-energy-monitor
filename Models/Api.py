abee3040ec2e310f3a5092a2cf34db49ad649812
Raúl Caro Pastorino <raul@fryntiz.dev>
2022-10-27 18:45:00 +0200

    Declare base methods in Api class

--- Models/Api.py
+++ Models/Api.py
@@ -5,3 +5,15 @@
 class Api():
     def __init__(self):
         pass
+
+    def getDevicesInfo(self):
+        """Get devices info from API"""
+        pass
+
+    def postReadings(self, readings):
+        """Post readings to API"""
+        pass
+
+    def debug(self, msg):
+        """Debug Api connection to console"""
+        pass
