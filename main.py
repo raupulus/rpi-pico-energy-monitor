e77c409111abc5293ccf54d96a84f90c95893f55
Raúl Caro Pastorino <raul@fryntiz.dev>
2022-10-29 20:19:15 +0200

    Optmize and extract old functions to method class

--- main.py
+++ main.py
@@ -1,73 +1,36 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-#
-
 from machine import Pin, ADC, SPI
 from time import sleep
 import utime
 from Models.DisplayST7735_128x160 import DisplayST7735_128x160
+from Models.Sensor_Intensity import Sensor_Intensity
 from Models.Sensor_Voltage import Sensor_Voltage
-
-# Sensor de voltage
-SENSOR_VOLTAGE = Sensor_Voltage(28)
-
-cs = machine.Pin(13, machine.Pin.OUT)
-reset = machine.Pin(9, machine.Pin.OUT)
-
-spi1 = machine.SPI(1, baudrate=8000000, polarity=0, phase=0,
-                   firstbit=machine.SPI.MSB, sck=machine.Pin(10), mosi=machine.Pin(11), miso=None)
-
-# Pantalla principal 128x160px
-display = DisplayST7735_128x160(spi1, rst=9, ce=13, dc=12)
-
+from Models.RpiPico import RpiPico
 
 voltage_working = 3.3
-adc_conversion_factor = voltage_working / 65535  # 16/bits: raspberry 12bits.
+
 # Corrección de voltaje al leer ADC, en rpi pico según datasheet es 0.706
 adc_voltage_correction = 0.706
 
-INTEGRATED_TEMP_CORRECTION = 27  # Temperatura interna para corregir lecturas
+# Rpi Pico Model
+controller = RpiPico()
 
-# Definición de GPIO
-LED_INTEGRATED = Pin("LED", Pin.OUT)
+# Sensor de voltage
+SENSOR_VOLTAGE = Sensor_Voltage(
+    28, 0.5, voltage_working, adc_voltage_correction)
 
+# Sensores de intensidad
+SENSOR_INTENSITY_1 = Sensor_Intensity(27, 0.1, 0.20, voltage_working)
+SENSOR_INTENSITY_2 = Sensor_Intensity(26, 0.66, 0.20, voltage_working)
 
-# Pines analógicos
-adc0 = machine.ADC(26)  # Sensor de intensidad.
-adc1 = machine.ADC(27)  # Sensor de intensidad.
-adc2 = machine.ADC(28)  # Sensor de tensión.
-adc3 = machine.ADC(4)  # Sensor interno de raspberry pi pico.
+# Pantalla principal 128x160px
+cs = Pin(13, Pin.OUT)
+reset = Pin(9, Pin.OUT)
 
-
-# Guardo estadísticas
-
-max_voltage = 0
-max_intensity = 0
-max_cpu_temp = 0
-min_voltage = 0
-min_intensity = 0
-min_cpu_temp = 0
-avg_voltage = 0
-avg_intensity = 0
-avg_cpu_temp = 0
+spi1 = SPI(1, baudrate=8000000, polarity=0, phase=0,
+           firstbit=SPI.MSB, sck=Pin(10), mosi=Pin(11), miso=None)
 
 
-def getRpiPicoTemp():
-    global max_cpu_temp, min_cpu_temp, avg_cpu_temp
-
-    reading = (adc3.read_u16() * adc_conversion_factor) - \
-        adc_voltage_correction
-    cpu_temp = INTEGRATED_TEMP_CORRECTION - reading / \
-        0.001721  # Formula given in RP2040 Datasheet
-
-    # Estadísticas
-    if cpu_temp > max_cpu_temp:
-        max_cpu_temp = cpu_temp
-    if cpu_temp < min_cpu_temp:
-        min_cpu_temp = cpu_temp
-    avg_cpu_temp = (avg_cpu_temp + cpu_temp) / 2
-
-    return cpu_temp
+display = DisplayST7735_128x160(spi1, rst=9, ce=13, dc=12)
 
 
 def setReadOn():
@@ -75,7 +38,7 @@ def setReadOn():
     Preparar lectura, enciende el led integrado.
     TODO: Iniciar conexión al wifi, para que cargue mientras estamos leyendo datos.
     """
-    LED_INTEGRATED.on()
+    controller.ledOn()
 
 
 def setReadOff():
@@ -84,82 +47,50 @@ def setReadOff():
     TODO: Al terminar la lectura, enviar datos por wifi a la API.
     """
 
-    LED_INTEGRATED.off()
-
-
-def getIntensity(samples, sensibility_5v=0.1):
-    """
-    Recibe la sensibilidad para 5v, se calcula por si la tensión de trabajo es otra.
-    """
-
-    global max_intensity, min_intensity, avg_intensity
-
-    # Revisar: La sensibilidad, de 100mv/a es para 5v. Sacar método para pasarlo a 3,3v
-    sensibility = ((sensibility_5v / 5) * voltage_working) * voltage_working
-
-    sum = 0  # Almaceno el total de valores a partir de todos los samples
-
-    for read in range(samples):
-        reading = adc2.read_u16()  # Lectura del ADC a 16 bits
-        readingParse = ((reading - adc_voltage_correction)
-                        * adc_conversion_factor)
-
-        if readingParse <= (voltage_working/2):
-            readingParse = voltage_working/2
-
-        sum += readingParse
-
-    average = sum/samples
-
-    # Calcula la intensidad en ma?
-    intensity = (average - (voltage_working/2)) / sensibility
-
-    # Estadísticas
-    if intensity > max_intensity:
-        max_intensity = (intensity / 10)
-    if intensity < min_intensity:
-        min_intensity = (intensity / 10)
-    avg_intensity = (avg_intensity + (intensity / 10)) / 2
-
-    return intensity/10
+    controller.ledOff()
 
 
 while True:
     setReadOn()
 
-    voltage = SENSOR_VOLTAGE.getStats(200)
-    intensity = round(getIntensity(50, 0.1), 4)
-    cpu_temp = round(getRpiPicoTemp(), 2)
+    cpu = controller.getStats()
+    voltage = SENSOR_VOLTAGE.getStats(50)
+    intensity1 = SENSOR_INTENSITY_1.getStats(500)
+    intensity2 = SENSOR_INTENSITY_2.getStats(500)
 
-    display.printStat(1, 'VCC/AVG: ', str(voltage.get('current')
-                                          ) + '/' + str(voltage.get('avg')), 'V')
+    display.printStat(1, 'CPU/MAX: ', str(cpu.get('current')
+                                          ) + '/' + str(cpu.get('max')), 'C')
 
-    display.printStat(2, 'V.MIN/V.MAX: ', str(voltage.get('min')
-                                              ) + '/' + str(voltage.get('max')), 'V')
+    display.printStat(
+        3, 'VCC/AVG: ', str(voltage.get('current')) + '/' + str(voltage.get('avg')), 'V')
 
-    display.printStat(3, 'Int: ', round(intensity, 2), 'A')
-    display.printStat(4, 'Cpu: ', round(cpu_temp, 2), 'C')
+    display.printStat(
+        4, 'V.MIN/V.MAX: ', str(voltage.get('min')) + '/' + str(voltage.get('max')), 'V')
 
-    display.printStat(5, '', '', '')
-    #display.printStat(5, 'Voltaje AVG: ', voltage.get('avg'), 'V')
-    display.printStat(6, 'Intensidad AVG: ', round(avg_intensity, 2), 'A')
+    display.printStat(
+        6, 'I1/AVG: ', str(intensity1.get('current')) + '/' + str(intensity1.get('avg')), 'A')
 
-    display.printStat(7, '', '', '')
-    #display.printStat(8, 'Voltaje Min.: ', voltage.get('min'), 'V')
-    display.printStat(8, 'Intensidad Min: ', round(min_intensity, 2), 'A')
-    #display.printStat(10, 'Voltaje Max.: ', voltage.get('max'), 'V')
-    display.printStat(9, 'Intensidad Max: ', round(max_intensity, 2), 'A')
+    display.printStat(
+        7, 'I1.MIN/I1.MAX: ', str(intensity1.get('min')) + '/' + str(intensity1.get('max')), 'A')
+
+    display.printStat(
+        9, 'I2/AVG: ', str(intensity2.get('current')) + '/' + str(intensity2.get('avg')), 'A')
+
+    display.printStat(
+        10, 'I2.MIN/I2.MAX: ', str(intensity2.get('min')) + '/' + str(intensity2.get('max')), 'A')
+
+    display.printStat(8, '', '', '')
 
     print()
+    print('cpu_temp: ' + str(cpu.get('current')) + ' C')
     print('voltage: ' + str(voltage.get('current')) + ' V')
-    print('intensity: ' + str(intensity) + ' A')
-    print('cpu_temp: ' + str(cpu_temp) + ' C')
+    print('intensity1: ' + str(intensity1.get('current')) + ' A')
+    print('intensity2: ' + str(intensity2.get('current')) + ' A')
+    #print('DEBUG I1: ' + str(SENSOR_INTENSITY_1.debug_max_voltage_on_disconnect))
+    #print('DEBUG I2: ' + str(SENSOR_INTENSITY_2.debug_max_voltage_on_disconnect))
     print()
-
-    # TODO: Después de cada subida a la API, resetear contador de estadísticas.
-    # SENSOR_VOLTAGE.resetStats()
 
     utime.sleep(0.5)
     setReadOff()
 
-    utime.sleep(1.5)
+    utime.sleep(3)
